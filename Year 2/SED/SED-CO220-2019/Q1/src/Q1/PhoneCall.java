c91ce1fc1bfae17f78d581c42d5337b96cebdd4f
Ryan OD <58502747+Ryan7od@users.noreply.github.com>
2025-04-25 11:27:04 +0100

    feat: Complete 2019 Q1

--- Year 2/SED/SED-CO220-2019/Q1/src/Q1/PhoneCall.java
+++ Year 2/SED/SED-CO220-2019/Q1/src/Q1/PhoneCall.java
@@ -8,12 +8,18 @@ public class PhoneCall {
 
   private static final long PEAK_RATE = 25;
   private static final long OFF_PEAK_RATE = 10;
+  private final LocalTime PEAK_START_TIME = LocalTime.of(9, 00);
+  private final LocalTime PEAK_END_TIME = LocalTime.of(18, 00);
 
   private final String caller;
   private final String callee;
 
   private LocalTime startTime;
   private LocalTime endTime;
+
+  private long totalCharge = 0;
+
+
 
   public PhoneCall(String caller, String callee) {
     this.caller = caller;
@@ -23,17 +29,33 @@ public class PhoneCall {
   public void start() {
     startTime = LocalTime.now();
   }
+  public void start(LocalTime time) {
+    startTime = time;
+  }
 
   public void end() {
     endTime = LocalTime.now();
   }
-
-  public void charge() {
-    BillingSystem.getInstance().addBillItem(caller, callee, priceInPence());
+  public void end(LocalTime time) {
+    endTime = time;
   }
 
+  public void charge() {
+    long charge = priceInPence();
+    BillingSystem.getInstance().addBillItem(caller, callee, charge);
+    totalCharge += charge;
+  }
+  public long getTotalCharge() { return totalCharge; }
+
+  public long getPeakRate()    { return PEAK_RATE; }
+  public long getOffPeakRate() { return OFF_PEAK_RATE; }
+
+  public LocalTime getPeakStartTime() { return PEAK_START_TIME; }
+  public LocalTime getPeakEndTime() { return PEAK_END_TIME; }
+
   private long priceInPence() {
-    if (startTime.isAfter(LocalTime.of(9, 00)) && endTime.isBefore(LocalTime.of(18, 00))) {
+    if (startTime.isAfter(PEAK_START_TIME) && startTime.isBefore(PEAK_END_TIME)
+     || endTime.isBefore(PEAK_END_TIME) && endTime.isAfter(PEAK_START_TIME)) {
       return duration() * PEAK_RATE;
     } else {
       return duration() * OFF_PEAK_RATE;
--- Year 2/SED/SED-CO220-2019/Q1/test/Q1/PhoneCallTest.java
+++ Year 2/SED/SED-CO220-2019/Q1/test/Q1/PhoneCallTest.java
@@ -1,6 +1,11 @@
 package Q1;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.Assert.*;
+
+import java.time.LocalTime;
+import static java.time.temporal.ChronoUnit.MINUTES;
 
 public class PhoneCallTest {
 
@@ -11,10 +16,82 @@ public class PhoneCallTest {
 
     call.start();
 
-    waitForSeconds(150);
+    waitForSeconds(5);
 
     call.end();
     call.charge();
+  }
+
+  @Test
+  public void chargesPeakCallCorrectly() throws Exception {
+    PhoneCall call = new PhoneCall("+447770123456", "+4479341554433");
+
+    LocalTime startTime = LocalTime.of(9, 15);
+    LocalTime endTime = LocalTime.of(9, 17);
+
+    Assert.assertTrue(startTime.isAfter(call.getPeakStartTime()));
+    Assert.assertTrue(endTime.isAfter(startTime));
+    Assert.assertTrue(endTime.isBefore(call.getPeakEndTime()));
+
+    call.start(startTime);
+    call.end(endTime);
+    call.charge();
+
+    Assert.assertEquals(call.getTotalCharge(), call.getPeakRate() * (MINUTES.between(startTime, endTime) + 1));
+  }
+
+  @Test
+  public void chargesStartPeakEndOffPeakCallCorrectly() throws Exception {
+    PhoneCall call = new PhoneCall("+447770123456", "+4479341554433");
+
+    LocalTime startTime = LocalTime.of(17, 58);
+    LocalTime endTime = LocalTime.of(18, 07);
+
+    Assert.assertTrue(startTime.isAfter(call.getPeakStartTime()));
+    Assert.assertTrue(endTime.isAfter(startTime));
+    Assert.assertTrue(endTime.isAfter(call.getPeakEndTime()));
+
+    call.start(startTime);
+    call.end(endTime);
+    call.charge();
+
+    Assert.assertEquals(call.getTotalCharge(), call.getPeakRate() * (MINUTES.between(startTime, endTime) + 1));
+  }
+
+  @Test
+  public void chargesStartOffPeakEndPeakCallCorrectly() throws Exception {
+    PhoneCall call = new PhoneCall("+447770123456", "+4479341554433");
+
+    LocalTime startTime = LocalTime.of(8, 58);
+    LocalTime endTime = LocalTime.of(9, 07);
+
+    Assert.assertTrue(startTime.isBefore(call.getPeakStartTime()));
+    Assert.assertTrue(endTime.isAfter(startTime));
+    Assert.assertTrue(endTime.isBefore(call.getPeakEndTime()));
+
+    call.start(startTime);
+    call.end(endTime);
+    call.charge();
+
+    Assert.assertEquals(call.getTotalCharge(), call.getPeakRate() * (MINUTES.between(startTime, endTime) + 1));
+  }
+
+  @Test
+  public void chargesOffPeakCallCorrectly() throws Exception {
+    PhoneCall call = new PhoneCall("+447770123456", "+4479341554433");
+
+    LocalTime startTime = LocalTime.of(18, 15);
+    LocalTime endTime = LocalTime.of(18, 17);
+
+    Assert.assertTrue(startTime.isAfter(call.getPeakEndTime()));
+    Assert.assertTrue(endTime.isAfter(startTime));
+    Assert.assertTrue(endTime.isBefore(LocalTime.MAX));
+
+    call.start(startTime);
+    call.end(endTime);
+    call.charge();
+
+    Assert.assertEquals(call.getTotalCharge(), call.getOffPeakRate() * (MINUTES.between(startTime, endTime) + 1));
   }
 
   private void waitForSeconds(int n) throws Exception {
